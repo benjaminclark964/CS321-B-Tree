ae65127e53986fb632a75bd24e0bce664b3e20de
juanbecerra0 <juanbecerra@u.boisestate.edu>
2019-04-11 18:58:20 -0600

    Implemented TreeObject.java

--- TreeObject.java
+++ TreeObject.java
@@ -1,4 +1,55 @@
-
+/**
+ * Object stored within a BTreeNode
+ * 
+ * Consists of a long value (represents a substring) and
+ * an integer value (represents the frequency)
+ *
+ */
 public class TreeObject {
 
+	//	Variables
+	private long dnaInput;
+	private int frequency;
+	
+	/**
+	 * Constructor for TreeObject
+	 */
+	public TreeObject (long dnaInput) {
+		this.setDnaInput(dnaInput);
+		this.setFrequency(1);
+	}
+
+	/**
+	 * Getter for DnaInput
+	 * @return
+	 */
+	public long getDnaInput() {
+		return dnaInput;
+	}
+
+	/**
+	 * Setter for dnaInput
+	 * @param dnaInput
+	 */
+	public void setDnaInput(long dnaInput) {
+		this.dnaInput = dnaInput;
+	}
+
+	/**
+	 * Getter for frequency
+	 * @return
+	 */
+	public int getFrequency() {
+		return frequency;
+	}
+
+	/**
+	 * Setter for frequency
+	 * @param frequency
+	 */
+	public void setFrequency(int frequency) {
+		this.frequency = frequency;
+	}
+	
+	
 }
