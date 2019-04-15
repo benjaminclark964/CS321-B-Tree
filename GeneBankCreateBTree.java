bf44d8ce17bcba96f268fac683d4f6b47d0f4aa6
juanbecerra0 <juanbecerra@u.boisestate.edu>
2019-04-15 14:45:07 -0600

    Implemented some handling for invalid arguments

--- GeneBankCreateBTree.java
+++ GeneBankCreateBTree.java
@@ -29,6 +29,7 @@ public class GeneBankCreateBTree {
 		try {
 			//	Parse argument 
 			if(args.length == 4 || args.length == 6) {
+				//	Assign values to variables
 				usingCache = Integer.valueOf(args[0]);
 				degree = Integer.valueOf(args[1]);
 				gbkFile = new File(args[2]);
@@ -44,7 +45,45 @@ public class GeneBankCreateBTree {
 				}
 				
 				//	Ensure that arguments are valid
-				//	TODO
+				
+				//	usingCache must be 0 or 1
+				if(!(usingCache == 0) || !(usingCache == 1)) {
+					System.err.println("You must specify whether or not you are using a cache (0 or 1). You entered " + usingCache + ".\n");
+					printUsageAndExit();
+				}
+				
+				//	degree must be either 0 (becomes 4096 bytes) or some positive integer
+				if(degree == 0) {
+					degree = 4096;
+				}
+				if(!(degree > 0)) {
+					System.err.println("Your degree must be greater or equal to 0. You entered " + degree + ".\n");
+					printUsageAndExit();
+				}
+				
+				//	gbkFile must exist and must be a file
+				if(!(gbkFile.exists()) || !(gbkFile.isFile())) {
+					System.err.println("The file file (" + gbkFile.toString() + ") does not exist or could not be found.\n");
+					printUsageAndExit();
+				}
+				
+				//	sequenceLength must be between 1 and 31 (inclusive)
+				if(!(sequenceLength >= 1 && sequenceLength <= 31)) {
+					System.err.println("Your sequence length must be between 1 and 31 (inclusive). You entered " + sequenceLength + ".\n");
+					printUsageAndExit();
+				}
+				
+				//	Cache size must be greater than 0, but can be anything if usingCache is 0
+				if(usingCache == 1 && cacheSize <= 0) {
+					System.err.println("Your cache size must be greater than 0. You entered " + cacheSize + ".\n");
+					printUsageAndExit();
+				}
+				
+				//	Debug level must either be 0 or 1
+				if(!(debugLevel == 0 || debugLevel == 1)) {
+					System.err.println("Your debug level must either be 0 or 1. You entered " + debugLevel + ".\n");
+					printUsageAndExit();
+				}
 				
 				//	Pass variables to the BTree constructor
 				//	TODO
